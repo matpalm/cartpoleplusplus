6e3474358f960193ec2e6a7fb0af5915b7b62eff
mat kelcey <matthew.kelcey@gmail.com>
2016-08-18 22:41:38 -0700

    add epoch time to stats, if its there

--- parse_out.py
+++ parse_out.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import argparse, sys, re, json
+import argparse, datetime, json, re, sys
 import numpy as np
 
 parser = argparse.ArgumentParser()
@@ -7,17 +7,22 @@ parser.add_argument('files', metavar='F', type=str, nargs='+',
                     help='files to process')
 parser.add_argument('--pg-emit-all', action='store_true',
                     help="if set emit all rewards in pg case. if false just emit stats")
+parser.add_argument('--nth', type=int,
+                    help="if >0 only emit every nth record")
 opts = parser.parse_args()
 
 # fields we output
 # ones specific to policy gradient include ... r_XXX 
-print "\t".join(["run_id", "n", "episode", "reward", "loss", "r_min", "r_mean", "r_max"])
+print "\t".join(["epoch", "run_id", "n", "episode", "reward", "loss", 
+                 "r_min", "r_mean", "r_max"])
 
 # emit a record.
 n = 0
-def emit(run_id, episode, reward, loss, r_min=0, r_mean=0, r_max=0):
+def emit(epoch, run_id, episode, reward, loss, r_min=0, r_mean=0, r_max=0):
   global n
-  print "\t".join(map(str, [run_id, n, episode, reward, loss, r_min, r_mean, r_max]))
+  if opts.nth is None or n % opts.nth == 0:
+    print "\t".join(map(str, [epoch, run_id, n, episode, reward, loss,
+                              r_min, r_mean, r_max]))
   n += 1
 
 for filename in opts.files:
@@ -28,23 +33,35 @@ for filename in opts.files:
       # looks like entry from dqn_cartpole
       try:
         m = re.match(".*episode: (\d+),.*episode reward: (.*?),.*loss: (.*?),", line)
-        emit(run_id=run_id, episode=m.group(1), reward=m.group(2),
+        emit(epoch=0, run_id=run_id, episode=m.group(1), reward=m.group(2),
              loss="0" if m.group(3) == "--" else m.group(3))
       except AttributeError:
         pass
     elif line.startswith("STATS"):      
       # looks like entry from the policy gradient code...
       try:
-        d = json.loads(re.sub("STATS.*?\t", "", line))
+        m = re.match("^STATS (.*?)\t(.*)", line)
+        if m is None:
+          continue  # some line stdin/stderr mixing...
+        dts, data = m.groups()
+        d = json.loads(data)
+        if 'epoch' not in d:
+          d['epoch'] = datetime.datetime.strptime(dts, '%Y-%m-%d %H:%M:%S').strftime('%s')
         if opts.pg_emit_all:
           for reward in d['rewards']:
-            emit(run_id=run_id, episode=d['batch'], reward=reward, loss=d['loss'],
-                 r_min=np.min(d['rewards']), r_mean=np.mean(d['rewards']),
+            emit(epoch=d['epoch'], run_id=run_id, episode=d['batch'], reward=reward, 
+                 loss=d['loss'], r_min=np.min(d['rewards']), r_mean=np.mean(d['rewards']),
                  r_max=np.max(d['rewards']))
         else:
-          emit(run_id=run_id, episode=d['batch'], reward=d['mean_reward'], loss=d['loss'],
-               r_min=np.min(d['rewards']), r_mean=np.mean(d['rewards']),
-               r_max=np.max(d['rewards']))
+          emit(epoch=d['epoch'], run_id=run_id, episode=d['batch'],
+               reward=d['mean_reward'], loss=d['loss'], r_min=np.min(d['rewards']),
+               r_mean=np.mean(d['rewards']), r_max=np.max(d['rewards']))
+#      except ValueError:
+#        pass  # partial line?
       except ValueError:
-        pass  # partial line?
+        # partial line? or mix of stdin / stderr (e.f. "not contained
+        # within observation space" )
+        pass  
+#      except AttributeError:
+#        print "????", line
       
