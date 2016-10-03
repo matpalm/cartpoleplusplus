4f293cb66ff50868ee7cead0d87c820bce37f2a2
mat kelcey <matthew.kelcey@gmail.com>
2016-10-03 08:18:39 -0700

    more wip on preloaded replay

--- event_log.py
+++ event_log.py
@@ -2,6 +2,7 @@
 import event_pb2
 import gzip
 import matplotlib.pyplot as plt
+import numpy as np
 import StringIO
 import struct
 
@@ -14,6 +15,34 @@ def png_to_rgb(png_bytes):
   # note PNG is always RGBA so we need to slice off A
   rgba = plt.imread(StringIO.StringIO(png_bytes))
   return rgba[:,:,:3]
+
+
+def add_state_to_event(state, event, use_raw_pixels):
+  """ serialises state and adds to event."""
+  if use_raw_pixels:
+    # TODO: be nice to have pose info here too in the pixel case...
+    for r in range(state.shape[2] / 3):  # 3 channels per action repeat
+      s = event.state.add()
+      s.render.width = state.shape[1]
+      s.render.height = state.shape[0]
+      s.render.png_bytes = rgb_to_png(state[:,:,3*r:3*(r+1)])
+  else:
+    for r in range(state.shape[0]):
+      s = event.state.add()
+      s.cart_pose.extend(map(float, state[r][0]))
+      s.pole_pose.extend(map(float, state[r][1]))  
+
+def read_state_from_event(event, use_raw_pixels):
+  """ reads serialised state from event and returns in form equiv to add_state_to_event"""
+  if use_raw_pixels:
+    raise Exception("todo")
+  else:
+    state = np.empty((len(event.state), 2, 7))
+    for i, s in enumerate(event.state):
+      state[i][0] = s.cart_pose
+      state[i][1] = s.pole_pose
+    return state
+  
 
 class EventLog(object):
 
@@ -35,18 +64,7 @@ class EventLog(object):
 
   def add(self, state, use_raw_pixels, done, action, reward):
     event = self.episode_entry.event.add()
-    if use_raw_pixels:
-      # TODO: be nice to have pose info here too in the pixel case...
-      for r in range(state.shape[2] / 3):  # 3 channels per action repeat
-        s = event.state.add()
-        s.render.width = state.shape[1]
-        s.render.height = state.shape[0]
-        s.render.png_bytes = rgb_to_png(state[:,:,3*r:3*(r+1)])
-    else:
-      for r in range(state.shape[0]):
-        s = event.state.add()
-        s.cart_pose.extend(map(float, state[r][0]))
-        s.pole_pose.extend(map(float, state[r][1]))
+    add_state_to_event(state, event, use_raw_pixels)
     event.is_terminal = done
     if isinstance(action, int):
       event.action.append(action)  # single action
@@ -108,6 +126,10 @@ if __name__ == "__main__":
     if opts.echo:
       print "-----", episode_id
       print episode
+      # > DEBUG
+      for event in episode.event:
+        print read_state_from_event(event, False)
+      # < DEBUG
     total_num_read_episodes += 1
     total_num_read_events += len(episode.event)
     if opts.img_output_dir is not None:
