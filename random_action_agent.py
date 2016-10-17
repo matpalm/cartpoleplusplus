f46002d402373835db6b805ed6af73e91ad1f036
mat kelcey <matthew.kelcey@gmail.com>
2016-10-16 21:45:31 -0700

    emit total_reward, not steps, in random_action_agent

--- random_action_agent.py
+++ random_action_agent.py
@@ -27,17 +27,19 @@ env = bullet_cartpole.BulletCartpole(opts=opts, discrete_actions=discrete_action
 for _ in xrange(opts.num_eval):
   env.reset()
   done = False
+  total_reward = 0
   steps = 0
   while not done:
     if discrete_actions:
       action = random.choice(actions)
     else:
       action = env.action_space.sample()
-    _state, _reward, done, info = env.step(action)
+    _state, reward, done, info = env.step(action)
     steps += 1
+    total_reward += reward
     if opts.max_episode_len is not None and steps > opts.max_episode_len:
       break
-  print steps
+  print total_reward
 
 env.reset()  # hack to flush last event log if required
 
