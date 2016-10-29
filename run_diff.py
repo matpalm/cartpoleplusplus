c8212fe2deb3657d28e034bc58791c96d68ea45c
mat kelcey <matthew.kelcey@gmail.com>
2016-10-28 22:13:01 -0700

    run-diff update - ignore keys always different in run-diff - support new keys

--- run_diff.py
+++ run_diff.py
@@ -17,11 +17,12 @@ def config(f):
             # value is a string, need special handling since it might contain a comma!
             m = re.match("^((.*?)='(.*?)',\s+)", config)
             pair, key, value = m.groups()
-          c[key] = value
         else:
           # final entry  
           pair = config
           key, value = config.split("=")
+        # ignore run related keys
+        if key not in ['ckpt_dir', 'event_log_out']:
           c[key] = value
         config = config.replace(pair, "")
       return c
@@ -30,14 +31,18 @@ def config(f):
 c1 = config(sys.argv[1])
 c2 = config(sys.argv[2])
 
+#unset_defaults = {"use_dropout": "False"}
+
 data = []
 max_widths = [0,0,0]
 for key in sorted(set(c1.keys()).union(c2.keys())):
-  data.append((key, c1[key], c2[key]))
+  c1v = c1[key] if key in c1 else " "#unset_defaults[key]
+  c2v = c2[key] if key in c2 else " "#unset_defaults[key]
+  data.append((key, c1v, c2v))
   max_widths[0] = max(max_widths[0], len(key))
-  max_widths[1] = max(max_widths[1], len(c1[key]))
-  max_widths[2] = max(max_widths[2], len(c2[key]))
+  max_widths[1] = max(max_widths[1], len(c1v))
+  max_widths[2] = max(max_widths[2], len(c2v))
 for k, c1, c2 in data:
   format_str = "%%%ds %%%ds %%%ds %%s" % tuple(max_widths)
-  star = "*" if c1 != c2 else " "
+  star = " ***" if c1 != c2 else " "
   print format_str % (k, c1, c2, star)
